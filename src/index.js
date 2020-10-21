40edf873fe208365a851d9743e8cbe2146b26992
HelmutSchulz <palysaieu@gmail.com>
2020-10-21 18:08:02 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n);
+  n = String(n);
+  var newString = "";
+  for (m = n.length - 1; m >= 0; m--) {
+      newString += n[m];
+  }
+  return newString;
 }
